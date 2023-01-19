52a30547b08cc852b5aca722ecdf70e842c5b721
sean-kim <sean.kim@daangnpay.com>
2023-01-19 12:29:09 +0900

    Feat: validatedParamJob 등록
    program arguments: --spring.batch.job.names=validatedParamJob -fileName=test.csv

---
+++ src/main/java/study/spring/SpringBatchBasic/job/ValidatedParamJobConfig.java
@@ -0,0 +1,51 @@
+package study.spring.SpringBatchBasic.job;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import study.spring.SpringBatchBasic.validator.FileParamValidator;
+
+@Configuration
+@RequiredArgsConstructor
+public class ValidatedParamJobConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job validatedParamJob(Step validatedParamStep) {
+        return jobBuilderFactory.get("validatedParamJob")
+                .incrementer(new RunIdIncrementer())
+                .validator(new FileParamValidator())
+                .start(validatedParamStep)
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step validatedParamStep(Tasklet validatedParamTasklet) {
+        return stepBuilderFactory.get("validatedParamStep")
+                .tasklet(validatedParamTasklet)
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public Tasklet validatedParamTasklet(@Value("#{jobParameters['fileName']}") String fileName) {
+        return (contribution, chunkContext) -> {
+            System.out.println("fileName:" + fileName);
+            return RepeatStatus.FINISHED;
+        };
+    }
+
+}
