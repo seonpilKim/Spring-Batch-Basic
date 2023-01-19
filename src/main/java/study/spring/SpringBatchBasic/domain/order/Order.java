6084b2f4f0d8becf7189e1f5fccd7b3f2703a0fd
sean-kim <sean.kim@daangnpay.com>
2023-01-19 16:54:59 +0900

    Chore: 생성자 추가 및 기본키 전략 세팅

--- src/main/java/study/spring/SpringBatchBasic/domain/order/Order.java
+++ src/main/java/study/spring/SpringBatchBasic/domain/order/Order.java
@@ -2,9 +2,12 @@ package study.spring.SpringBatchBasic.domain.order;
 
 import java.util.Date;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -13,10 +16,12 @@ import lombok.ToString;
 @Entity
 @ToString
 @Table(name = "orders")
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String orderItem;
     private Integer price;
