48152805355154b79aae35d27fd1ade3cff3e28e
sean-kim <sean.kim@daangnpay.com>
2023-01-19 15:48:58 +0900

    Feat: fileReadWriteJob 등록, mapper 추가
    program arguments: --spring.batch.job.names=fileReadWriteJob

---
+++ src/main/java/study/spring/SpringBatchBasic/domain/player/PlayerFieldSetMapper.java
@@ -0,0 +1,21 @@
+package study.spring.SpringBatchBasic.domain.player;
+
+import org.springframework.batch.item.file.mapping.FieldSetMapper;
+import org.springframework.batch.item.file.transform.FieldSet;
+import org.springframework.validation.BindException;
+
+public class PlayerFieldSetMapper implements FieldSetMapper<Player> {
+
+    @Override
+    public Player mapFieldSet(FieldSet fieldSet) throws BindException {
+        return Player.builder()
+                .id(fieldSet.readString(0))
+                .lastName(fieldSet.readString(1))
+                .firstName(fieldSet.readString(2))
+                .position(fieldSet.readString(3))
+                .birthYear(fieldSet.readInt(4))
+                .debutYear(fieldSet.readInt(5))
+                .build();
+    }
+
+}
---
+++ src/main/java/study/spring/SpringBatchBasic/job/FileDataReadWriteConfig.java
@@ -0,0 +1,94 @@
+package study.spring.SpringBatchBasic.job;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
+import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+import study.spring.SpringBatchBasic.domain.player.Player;
+import study.spring.SpringBatchBasic.domain.player.PlayerFieldSetMapper;
+import study.spring.SpringBatchBasic.domain.player.PlayerYears;
+
+@Configuration
+@RequiredArgsConstructor
+public class FileDataReadWriteConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job fileReadWriteJob(Step fileReadWriteStep) {
+        return jobBuilderFactory.get("fileReadWriteJob")
+                .incrementer(new RunIdIncrementer())
+                .start(fileReadWriteStep)
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step fileReadWriteStep(
+            ItemReader<Player> playerItemReader,
+            ItemProcessor<Player, PlayerYears> playerItemProcessor,
+            FlatFileItemWriter<PlayerYears> playerItemWriter) {
+        return stepBuilderFactory.get("fileReadWriteStep")
+                .<Player, PlayerYears>chunk(5)
+                .reader(playerItemReader)
+                .processor(playerItemProcessor)
+                .writer(playerItemWriter)
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public ItemProcessor<Player, PlayerYears> playerItemProcessor() {
+        return PlayerYears::of;
+    }
+
+    @Bean
+    @StepScope
+    public FlatFileItemReader<Player> playerItemReader() {
+        return new FlatFileItemReaderBuilder<Player>()
+                .name("playerItemReader")
+                .resource(new FileSystemResource("player.csv"))
+                .lineTokenizer(new DelimitedLineTokenizer())
+                .fieldSetMapper(new PlayerFieldSetMapper())
+                .linesToSkip(1)
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public FlatFileItemWriter<PlayerYears> playerItemWriter() {
+        BeanWrapperFieldExtractor<PlayerYears> fieldExtractor = new BeanWrapperFieldExtractor<>();
+        fieldExtractor.setNames(new String[]{"id", "lastName", "position", "yearsExperience"});
+        fieldExtractor.afterPropertiesSet();
+
+        DelimitedLineAggregator<PlayerYears> lineAggregator = new DelimitedLineAggregator<>();
+        lineAggregator.setDelimiter(",");
+        lineAggregator.setFieldExtractor(fieldExtractor);
+
+        FileSystemResource outputResource = new FileSystemResource("players_output.txt");
+
+        return new FlatFileItemWriterBuilder<PlayerYears>()
+                .name("playerItemWriter")
+                .resource(outputResource)
+                .lineAggregator(lineAggregator)
+                .build();
+    }
+
+}
