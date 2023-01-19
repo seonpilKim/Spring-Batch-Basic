795328f778d43432d309c16fcd6205a5936b7829
sean-kim <sean.kim@daangnpay.com>
2023-01-19 12:29:46 +0900

    Feat: multiple validators 적용

--- src/main/java/study/spring/SpringBatchBasic/job/ValidatedParamJobConfig.java
+++ src/main/java/study/spring/SpringBatchBasic/job/ValidatedParamJobConfig.java
@@ -1,5 +1,6 @@
 package study.spring.SpringBatchBasic.job;
 
+import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -7,6 +8,7 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.CompositeJobParametersValidator;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -26,9 +28,15 @@ public class ValidatedParamJobConfig {
     public Job validatedParamJob(Step validatedParamStep) {
         return jobBuilderFactory.get("validatedParamJob")
                 .incrementer(new RunIdIncrementer())
-                .validator(new FileParamValidator())
+                .validator(multipleValidator())
                 .start(validatedParamStep)
                 .build();
+    }
+
+    private CompositeJobParametersValidator multipleValidator() {
+        CompositeJobParametersValidator validator = new CompositeJobParametersValidator();
+        validator.setValidators(Arrays.asList(new FileParamValidator()));
+        return validator;
     }
 
     @Bean
