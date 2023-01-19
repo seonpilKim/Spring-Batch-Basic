4c1a22ade7d9c87a66496bad05be8327be7e139f
sean-kim <sean.kim@daangnpay.com>
2023-01-19 12:39:09 +0900

    Feat: jobListenerJob 등록
    program arguments: --spring.batch.job.names=jobListenerJob

---
+++ src/main/java/study/spring/SpringBatchBasic/job/JobListenerJobConfig.java
@@ -0,0 +1,47 @@
+package study.spring.SpringBatchBasic.job;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class JobListenerJobConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job jobListenerJob(Step jobListenerStep) {
+        return jobBuilderFactory.get("jobListenerJob")
+                .incrementer(new RunIdIncrementer())
+                .start(jobListenerStep)
+                .build();
+    }
+    @Bean
+    @JobScope
+    public Step jobListenerStep(Tasklet jobListenerTasklet) {
+        return stepBuilderFactory.get("jobListenerStep")
+                .tasklet(jobListenerTasklet)
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public Tasklet jobListenerTasklet() {
+        return (contribution, chunkContext) -> {
+            System.out.println("Job Listener Tasklet");
+            return RepeatStatus.FINISHED;
+        };
+    }
+
+}
